eb9fd24358ebb3482f8d532e5490626a822b2e3e
Alexander Stillesjö <alexander.stillesjo@umetrics.com>
2016-06-02 13:28:23 +0200

    Stuff

--- src/coordinate-grid-mixin.js
+++ src/coordinate-grid-mixin.js
@@ -1190,7 +1190,11 @@ dc.coordinateGridMixin = function (_chart) {
             .scaleExtent(_zoomScale)
             .size([_chart.width(), _chart.height()])
             .duration(_chart.transitionDuration());
-        _chart.root().call(_zoom);
+        _chart.root().call(_zoom)
+            .on('mousedown.zoom', null)
+            .on('touchstart.zoom', null)
+            .on('touchmove.zoom', null)
+            .on('touchend.zoom', null);
     };
 
     _chart._disableMouseZoom = function () {
